4718ef4619ef778da9c59b59545b9c80afde9dd1
SesiSenaiDA2025 <sesisenaida2025@gmail.com>
2025-05-19 11:45:39 -0300

    Autenticação
    Autenticação incluída com sucesso.

--- 5_perfis_simples_alunos/controllers/AuthController.php
+++ 5_perfis_simples_alunos/controllers/AuthController.php
@@ -1,2 +1,43 @@
-<!-- 2ª Digitação (Aqui)
-Este arquivo será o responsável por: (Controlar a autenticação) -->
+<?php
+
+// Controlar a autenticação
+class AuthController {
+
+    // Exibir a página de login
+    public function login($erro = '') {
+        require_once 'views/login.php';
+        // Função que renderiza a tela
+        renderizarLogin($erro);
+    }
+
+    // Processa a tentativa de login
+    public function autenticar() {
+        // Obtem valor do formulário ou usa vazio
+        $usuario = $_POST['usuario'] ?? '';
+        $senha = $_POST['senha'] ?? '';
+
+        // Verifica se algum campo esta vazio
+        if (empty($usuario) || empty($senha)) {
+            $this->login('Preencha todos os campos!');
+        }
+
+        // Tenta autenticar chamando método "Auth"
+        $dadosUsuario = Auth::autenticar($usuario, $senha);
+
+        if($dadosUsuario) {
+            // Se credenciais válidas inicia sessão
+            Auth::iniciarSessao($dadosUsuario);
+
+            // Redireciona para página inicial (Protegida)
+            header('Location: index.php?pagina=lista');
+            exit;
+        } else {
+            $this->login('Usuário ou senha incorretos!');
+        }
+    }
+        // Processa o logout
+        public function logout() {
+            Auth::encerrarSessao();
+            header('Location: index.php?pagina=login');
+        }
+    }
--- 5_perfis_simples_alunos/index.php
+++ 5_perfis_simples_alunos/index.php
@@ -1,2 +1,38 @@
-<!-- 3ª Digitação (Aqui)
-Este arquivo será o responsável por: (Arquivo principal de controle) -->
+<?php
+
+// Arquivo principal sistema
+
+session_start();
+
+// Carrega arquivos necessários
+require_once 'config/database.php';
+require_once 'Services/Auth.php';
+require_once 'controllers/AuthController.php';
+
+// Criar objeto (Instanciar) Controle de autenticação
+$authController = new AuthController();
+
+// Ação padrão
+$pagina = $_GET['pagina'] ?? '';
+
+// Roteamento básico (Teste de autenticação)
+switch ($pagina) {
+    case 'login':
+        $authController->login();
+        break;
+    case 'autenticar':
+        $authController->autenticar();
+        break;
+    case 'logout':
+        $authController->logout();
+        break;
+    default:
+        if (Auth::estaLogado()) {
+            echo"Login bem-sucedido! Bem-vindo, " .Auth::obterUsuario()['nome'];
+            echo"<br><a href='index.php?pagina=logout'>Sair</a>";
+        } else {
+            header('Location: index.php?pagina=login');
+            exit;
+        }
+        break;
+}
